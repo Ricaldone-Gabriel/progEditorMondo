17b40c03c782045fa9941fbe2a704249b2e10675
gabriel.ricaldone <gabriel.ricaldone@aula3s-15.maxwell.local>
2023-03-29 09:30:17 +0200

    Modifica layout intero programma

--- src/progeditormondo/ProgEditorMondo.java
+++ src/progeditormondo/ProgEditorMondo.java
@@ -7,7 +7,9 @@ package progeditormondo;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -90,9 +92,10 @@ public class ProgEditorMondo extends JFrame {
 
         JPanel pannelloSceltaTerreno = new JPanel();
         JPanel pannelloMondo = new JPanel();
-
-        JLabel titoloMenu = new JLabel("Menù");
-
+        JPanel contenutoPannelloTerreno = new JPanel();
+        JPanel pannelloStrumenti = new JPanel();
+        JLabel titoloTerreno = new JLabel("Terreni");
+        
         JButton terra = new JButton("Terra");
         JButton acquaP = new JButton("Acqua profonda");
         JButton acquaS = new JButton("Acqua spiaggia");
@@ -101,53 +104,54 @@ public class ProgEditorMondo extends JFrame {
         JButton erbaC = new JButton("Erba chiara");
         JButton salva = new JButton("Salva");
         JButton carica = new JButton("Carica");
-
+        JButton cancella = new JButton("Cancella");
+        
         //Si salva su mondo.txt
         frame.setLayout(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.setBounds(0, 0, 1280, 720);
 
-        pannelloMondo.setBounds(0, 0, frame.getWidth() - 300, frame.getHeight());
+        pannelloStrumenti.setBounds(0,frame.getHeight()-179,frame.getWidth()-300,200);
+        pannelloStrumenti.setBackground(new Color(200,200,200));
+        pannelloStrumenti.setBorder(BorderFactory.createLoweredBevelBorder());
+        pannelloStrumenti.setLayout(new GridLayout(4,0));
+        
+        pannelloMondo.setBounds(0, 0, frame.getWidth() - 300, frame.getHeight() - 200);
         pannelloMondo.setLayout(null);
         pannelloMondo.setBackground(new Color(240, 240, 240));
+        
+        pannelloSceltaTerreno.setBounds(frame.getWidth() - 300, -1, 300, frame.getHeight());
+        pannelloSceltaTerreno.setBackground(new Color(200, 200, 200));
+        pannelloSceltaTerreno.setBorder(BorderFactory.createLoweredBevelBorder());
+        
+        contenutoPannelloTerreno.setLayout(new GridLayout(3,0));
+        
+        
         for (int i = 0; i < grandezzaX; i++) {
             for (int j = 0; j < grandezzaY; j++) {
                 pannelloMondo.add(mondoEditor[j][i]);
 
             }
         }
-        pannelloSceltaTerreno.setBounds(frame.getWidth() - 300, -1, 300, frame.getHeight());
-        pannelloSceltaTerreno.setBackground(new Color(200, 200, 200));
-        pannelloSceltaTerreno.setBorder(BorderFactory.createLoweredBevelBorder());
 
-        titoloMenu.setBorder(new EmptyBorder(10, 10, 30, 10));
-        titoloMenu.setFont(new Font("Calibri", Font.PLAIN, 30));
+        titoloTerreno.setBorder(new EmptyBorder(10, 10, 30, 10));
+        titoloTerreno.setFont(new Font("Calibri", Font.PLAIN, 30));
 
-        titoloMenu.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        terra.setAlignmentX(Component.CENTER_ALIGNMENT);
-        acqua.setAlignmentX(Component.CENTER_ALIGNMENT);
-        acquaP.setAlignmentX(Component.CENTER_ALIGNMENT);
-        acquaS.setAlignmentX(Component.CENTER_ALIGNMENT);
-        erba.setAlignmentX(Component.CENTER_ALIGNMENT);
-        erbaC.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        salva.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        carica.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        pannelloSceltaTerreno.add(titoloMenu);
-        pannelloSceltaTerreno.add(terra);
-        pannelloSceltaTerreno.add(acqua);
-        pannelloSceltaTerreno.add(acquaP);
-        pannelloSceltaTerreno.add(acquaS);
-        pannelloSceltaTerreno.add(erba);
-        pannelloSceltaTerreno.add(erbaC);
-        pannelloSceltaTerreno.add(salva);
-        pannelloSceltaTerreno.add(carica);
-
-        pannelloSceltaTerreno.setLayout(new BoxLayout(pannelloSceltaTerreno, BoxLayout.Y_AXIS));
+        titoloTerreno.setAlignmentX(Component.CENTER_ALIGNMENT);
+        pannelloSceltaTerreno.add(titoloTerreno);
+        pannelloSceltaTerreno.add(contenutoPannelloTerreno);
+        
+        contenutoPannelloTerreno.add(terra);
+        contenutoPannelloTerreno.add(acqua);
+        contenutoPannelloTerreno.add(acquaP);
+        contenutoPannelloTerreno.add(acquaS);
+        contenutoPannelloTerreno.add(erba);
+        contenutoPannelloTerreno.add(erbaC);
+        
+        pannelloStrumenti.add(salva);
+        pannelloStrumenti.add(carica);
+        pannelloStrumenti.add(cancella);
 
         terra.addActionListener(new ActionListener() {
             @Override
@@ -268,38 +272,25 @@ public class ProgEditorMondo extends JFrame {
             }
         }
         );
+        
+        cancella.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                for(int i = 0; i < grandezzaY; i++) {
+                    for(int j = 0; j < grandezzaX; j++) {
+                        mondo[i][j].terreno = "terra";
+                        mondoEditor[i][j].setBackground(new Color(155, 90, 60));
+                    }
+                }
+            }
+        });
 
         frame.add(pannelloSceltaTerreno);
-
+        frame.add(pannelloStrumenti);
         frame.add(pannelloMondo);
 
-        frame.setVisible(
-                true);
-        /*
-        
-        for (int i = 0; i < mondoSizeX; i++) {
-            for (int j = 0; j < mondoSizeY; j++) {
-                mondo[i][j] = new Piattaforma("terra", i, j);
-                switch (mondo[i][j].terreno) {
-                    case "terra":
-                        g.setColor(new Color(205, 133, 63));
-                        break;
-                    case "ghiaccio":
-                        g.setColor(new Color(173, 216, 230));
-                        break;
-                    default:
-                        g.setColor(Color.red);
-                        break;
-                }
-                g.fillRect(mondo[i][j].posX * mondo[i][j].size, mondo[i][j].posY * mondo[i][j].size, mondo[i][j].size, mondo[i][j].size);
-                g.setColor(Color.black);
-                g.drawRect(mondo[i][j].posX * mondo[i][j].size, mondo[i][j].posY * mondo[i][j].size, mondo[i][j].size, mondo[i][j].size);
-            }
-        }
-
-        g.fillRect((papa.posX * 50) + 5, (papa.posY * 50) + 5, papa.size, papa.size);
-        
-         */
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
